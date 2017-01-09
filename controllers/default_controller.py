cdff8027c5c92550d8c9bebfaa29cf47dbedd865
tannoa2 <azzam.tannous@emc.com>
2017-01-09 15:43:51 +0000

    add a Get /sys api to discover the HW managed by the USC

--- controllers/default_controller.py
+++ controllers/default_controller.py
@@ -1,6 +1,34 @@
 from ucsmsdk.ucshandle import UcsHandle
+import json
+
+logedin = True
+globalhandle = {}
 
 def login_get(host = None, user = None, password = None):
-    handle = UcsHandle(host, user, password);
+    global globalhandle
+    handle = UcsHandle(host, user, password,secure=False);
+    globalhandle = handle;
     if handle.login():
         return handle.cookie;
+
+
+
+def systemGetAll():
+    global globalhandle
+    if (logedin):
+        elements = [{"ciscoXmlName": "EquipmentChassis", "humanReadableName": "Chassis"},
+                    {"ciscoXmlName": "NetworkElement", "humanReadableName": "Fabric Interconnects"},
+                    {"ciscoXmlName": "EquipmentFex", "humanReadableName": "FEX"},
+                    {"ciscoXmlName": "computeRackUnit", "humanReadableName": "Servers"}]
+        finalObjs = []
+        for x in elements:
+            units = []
+            components = globalhandle.query_children(in_dn="sys", class_id=x["ciscoXmlName"])
+            componentObj = {
+                "name": x["humanReadableName"],
+                "members": units}
+            for y in components:
+                subElement = {"relative_path": "/"+(vars(y))["dn"]}
+                units.append(subElement)
+            finalObjs.append(componentObj)
+        return finalObjs
